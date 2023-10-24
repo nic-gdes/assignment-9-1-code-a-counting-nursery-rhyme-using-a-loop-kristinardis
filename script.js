b92a2f096e0ae68edba3c9383210a8afab2eaaba
kristinardis <kamcllvenna@nic.edu>
2023-10-24 08:47:01 -0700

    This was coded exactly as in example given
    I don't understand why the last portion wouldn't have a "if (count === 1)" segment, rather than "if(count > 0)"

---
+++ script.js
@@ -0,0 +1,41 @@
+/*Ten fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+Nine fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+Eight fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+Seven fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+Six fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+ Five fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+Four fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+Three fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+Two fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+One fat sausages, sizzling in a pan, one went pop and the other went bang!
+
+No fat sausages, sizzling in a pan*/
+
+var word = "fat sausages";
+    var count = 10;
+
+   while (count > 0) {
+      console.log(count + " " + word + "s" + " sizzling in the pan");
+      console.log("one went pop and the other went bang!");
+
+      count = count - 1;
+
+    if (count > 0); {
+        console.log(count + " " + word + "s" + " sizzling in the pan");
+    } else {
+        console.log("No fat sausages sizzling in a pan");
+       }
+   }
+      
+
+
+
